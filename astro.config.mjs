3fbe1c74720df08155f0bfded870c248dc7e38d9
Zach Alvstad <52245667+zda17@users.noreply.github.com>
2025-01-13 19:21:20 -0600

    deleting stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import compressor from "astro-compressor";
-import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
