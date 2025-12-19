0f330b18ca9e6f011f0bd6081d79a46da57fdc40
Zach Alvstad <52245667+zda17@users.noreply.github.com>
2025-12-18 21:01:20 -0600

    update favicon

--- src/pages/favicon.ico.ts
+++ src/pages/favicon.ico.ts
@@ -3,7 +3,7 @@ import sharp from "sharp";
 import ico from "sharp-ico";
 import path from "node:path";
 
-const faviconSrc = path.resolve("src/images/icon.png");
+const faviconSrc = path.resolve("src/images/logowhite.png");
 
 export const GET: APIRoute = async () => {
 
